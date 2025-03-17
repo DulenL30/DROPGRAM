3b4a0396ef9747129045c662fe7a6a30fc967078
DulenL30 <dulenvinjaya21@gmail.com>
2025-03-18 01:03:32 +0530

    Authentication.

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,22 @@
+import { View, Text } from 'react-native';
+import { styles } from '@/styles/auth.styles';
+import { COLORS } from '@/constants/theme';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+
+
+export default function login() {
+  return (
+    <View style={styles.container}>
+        <View style={styles.brandSection}>
+            <View style={styles. logoContainer}>
+                <FontAwesome5 name="fire" size={42} color={COLORS.primary} />
+            </View>
+            <Text style={styles.appName}>Dropgram</Text>
+            <Text style={styles. tagline}>Cuba</Text>
+        </View>
+
+    </View>
+  )
+}
+
+
